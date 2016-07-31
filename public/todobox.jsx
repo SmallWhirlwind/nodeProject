e13e7ef75aa8f4b5031e46e7e81c10939c9b2e77
mahong <1025383143@qq.com>
2016-07-31 16:07:40 +0800

    部分

--- public/todobox.jsx
+++ public/todobox.jsx
@@ -1,11 +1,75 @@
+let style = {
+    checkedTodo: {
+        textDecoration: "line-through"
+    },
+    notCheckedTodo: {
+        textDecoration: "none"
+    }
+    // tableContent: {
+    //     border: "1px solid black"
+    // }
+};
+
+var Todo = React.createClass({
+
+    getInitialState: function () {
+        return {checked: false, TodoStyle: style.notCheckedTodo};
+    },
+    handleChange(e) {
+        this.setState({
+            checked: e.target.checked
+        });
+        if (e.target.checked) {
+            this.setState({
+                TodoStyle: style.checkedTodo
+            });
+        } else {
+            this.setState({
+                TodoStyle: style.notCheckedTodo
+            });
+        }
+    },
+
+    // _onDelete:function() {
+    //     this.props.onDelete(this.props.children);
+    // },
+
+    render: function () {
+        return (
+            <tr style={this.state.TodoStyle}>
+                <td>
+                    <input type="checkbox" checked={this.state.checked} onChange={this.handleChange}/>
+                </td>
+                <td >
+                    {this.props.children}
+                </td>
+                <td >
+                    <button onClick={this._onDelete}>X</button>
+                </td>
+            </tr>)
+    }
+});
 
 
 var TodoList = React.createClass({
+
+    // deleteTodo:function(id) {
+    //     let newData = this.state.items.filter(function (item) {
+    //         return item.id !== id;
+    //     });
+    //     this.setState({items: newData});
+    // },
+
     render: function () {
         var createItem = function (item) {
-            return <li key={item.id}>{item.text}</li>;
+            // return <li key={item.id}>{item.text}</li>;
+            return <Todo key={item.id} >{item.text}</Todo>
         };
-        return <div>{this.props.items.map(createItem)}</div>;
+        return <div>
+            <table>
+                <tbody>{this.props.items.map(createItem)}</tbody>
+            </table>
+        </div>;
     }
 });
 
