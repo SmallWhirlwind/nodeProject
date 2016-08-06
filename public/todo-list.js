12b6f33e4e274c310a3a4915bb4239dd8c67c28a
mahong <1025383143@qq.com>
2016-08-06 23:11:07 +0800

    add header

--- public/todo-list.js
+++ public/todo-list.js
@@ -1,10 +1,50 @@
 var App = React.createClass({
+
+    getInitialState: function () {
+        return {
+            itemsTodo: []
+        }
+    },
+    addTodo: function (newTodoItem) {
+        const itemsTodo = this.state.itemsTodo;
+        itemsTodo.push(newTodoItem);
+        this.setState(itemsTodo);
+        console.log(itemsTodo);
+    },
     render: function () {
         return (
-        <div>App</div>
+            <div>
+                <TodoHeader addTodo={this.addTodo}/>
+            </div>
 
         )
     }
 });
 
-ReactDOM.render(<App/>,document.getElementById("content"));
+var TodoHeader = React.createClass({
+    handlerKeyUp: function (event) {
+        if (event.keyCode === 13) {
+            let value = event.target.value;
+
+            if (!value) return false;
+
+            let newTodoItem = {
+                text: value,
+                isDone: false
+            };
+            event.target.value = "";
+            this.props.addTodo(newTodoItem);
+        }
+    },
+
+    render: function () {
+        return (
+            <div>
+                <input onKeyUp={this.handlerKeyUp.bind(this)} type="text" placeholder="what's your task ?"/>
+            </div>
+
+        )
+    }
+});
+
+ReactDOM.render(<App/>, document.getElementById("content"));
